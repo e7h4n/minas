1dc89cfac1b8b50450b5dc00c9f0d9e0a37d3b1d
zhangyc <zhangyc@fenbi.com>
2016-06-14 19:46:54 +0800

    enable web server

--- src/main/java/com/jinyufeili/minas/Application.java
+++ src/main/java/com/jinyufeili/minas/Application.java
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setWebEnvironment(true);
+        application.run(args);
     }
 }
